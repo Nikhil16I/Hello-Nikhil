873b897f3a92e14ef7ed87c084c7276593dfc93c
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-02-16 22:42:10 +0530

    One-Way Event  Binding Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello_Nikhil';
-  imgUrl="../assets/BL_logo_square_png"
+  imgUrl="../assets/BL_logo_square_png.png"
+  url= "https://www.bridgelabz.com"
 
   ngOnInit():void{
-    this.title= "Hello From Nikhil."
+    this.title= "Hello From Nikhil To Bridgelabz"
   }//Interpolation Data Binding 
+
+onClick($event: any){
+  console.log("Save Buuton Clicked !!", $event);
+  window.open(this.url,"blank");
+  }
 }
