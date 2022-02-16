608af903661846c62e8f15cc331ce0800dcb7308
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-02-16 23:42:18 +0530

    Showing Error For Invalid Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   imgUrl=""
 
   userName: string ="";//Adding userName as user Input/Typescripting
-
+  nameError: string ="";//Checking whether name is Correct
   ngOnInit():void{
     this.title= "Hello From Nikhil To Bridgelabz"
     this.imgUrl="../assets/BL_logo_square_png.png"
@@ -24,4 +24,14 @@ onClick($event: any){
   console.log("Save Buuton Clicked !!", $event);
   window.open(this.url,"blank");
   }//Event Binding 
+
+onInput($event: any){
+  console.log("Change Event Occured !",$event.data);
+  const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  if (nameRegex.test(this.userName)){
+    this.nameError=" ";
+    return;
+  }
+  this.nameError=" Invalid Name, Please Input Correct Name"
+}
 }
